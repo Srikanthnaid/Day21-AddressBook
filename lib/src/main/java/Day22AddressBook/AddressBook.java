cdcacc4561b9778c50b5babf839039704b936604
Srikanthnaid <ksru044gmail.com>
2022-12-07 07:10:26 +0530

    Welcome to addressbook problem

---
+++ lib/src/main/java/Day22AddressBook/AddressBook.java
@@ -0,0 +1,16 @@
+package Day22AddressBook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+	// uc1 -> create addressbook
+
+	// create a array-list
+	List<ContactList> contact = new ArrayList<ContactList>();
+
+	public void addcontact() {
+
+	}
+
+}
