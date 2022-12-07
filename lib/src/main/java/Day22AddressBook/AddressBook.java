e0652167cade94d8090bda7ad71daedfa400a765
Srikanthnaid <ksru044gmail.com>
2022-12-07 07:46:27 +0530

    Added: Create contact in addressbook

--- lib/src/main/java/Day22AddressBook/AddressBook.java
+++ lib/src/main/java/Day22AddressBook/AddressBook.java
@@ -9,8 +9,24 @@ public class AddressBook {
 	// create a array-list
 	List<ContactList> contact = new ArrayList<ContactList>();
 
-	public void addcontact() {
+	// addcontact method using to add the contact of addressbook
+	public void createContact() {
+		// here we create a object of contact-list class
+		ContactList list = new ContactList("Srikanth", "Naidu", "Alampur", "Hyderabad", "Telangana", 143, 63016630,
+				"ksru044@gmail.com");
 
+		/* adding contact to list */
+		contact.add(list);
+
+		// print the addressbook
+		contact.stream().forEach(a -> System.out.println(a));
 	}
 
+	public static void main(String[] args) {
+		System.out.println("--------------Welcome to addressbook--------------");
+
+		// create a object of addressbook
+		AddressBook book = new AddressBook();
+		book.createContact();
+	}
 }
